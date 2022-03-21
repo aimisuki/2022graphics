1ca44853ca84fa0241c254cfbe17be08838953af
aimisuki <hur891227@gmil.com>
2022-03-21 16:44:53 +0800

    week05

--- week05_mouse rotate/main.cpp
+++ week05_mouse rotate/main.cpp
@@ -1,13 +1,24 @@
 #include <GL/glut.h>
+float angle=0, oldX=0;
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glPushMatrix();
-        glRotatef(180, 0,0,1);
+        glRotatef(angle, 0,0,1);
         glColor3f(1,1,0);
         glutSolidTeapot(0.3);
     glPopMatrix();
     glutSwapBuffers();
+}
+void mouse(int button, int state, int x, int y)
+{
+    oldX=x;
+}
+void motion(int x, int y)
+{
+    angle += (x-oldX);
+    oldX = x;
+    display();
 }
 
 int main(int argc, char**argv){
@@ -16,6 +27,8 @@ int main(int argc, char**argv){
     glutCreateWindow("week05 Rotate");
 
     glutDisplayFunc( display );
+    glutMouseFunc(mouse);
+    glutMotionFunc(motion);
 
     glutMainLoop();
     return 0;
